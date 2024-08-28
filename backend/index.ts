48a98dc1e00009a9b6c6a2d7311bc78ceb9a3e53
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2024-08-28 18:13:25 +0200

    backend: Server running and prisma configurated

---
+++ backend/index.ts
@@ -0,0 +1,20 @@
+// import * as dotenv from "dotenv";
+// dotenv.config();
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+// import cors from "cors";
+// import router from "./router";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get("/tasks", async (req, res) => {
+	const tasks = await prisma.task.findMany();
+	res.json(tasks);
+});
+
+app.listen(3000, () => {
+	console.log("Server is running on http://localhost:3000");
+});
