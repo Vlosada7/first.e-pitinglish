08e9f7319b8a294bf070eaf0a9f7414592c4537c
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2024-08-28 20:13:26 +0200

    test

--- backend/index.ts
+++ backend/index.ts
@@ -15,6 +15,8 @@ app.get("/tasks", async (req, res) => {
 	res.json(tasks);
 });
 
+//Apenas para testar uma parada 
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
