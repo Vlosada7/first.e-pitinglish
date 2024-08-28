6ca63aef186a67c34c0e4c3b170a552525b07ca7
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2024-08-28 19:43:51 +0200

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -15,6 +15,6 @@ app.get("/tasks", async (req, res) => {
 	res.json(tasks);
 });
 
-app.listen(3000, () => {
-	console.log("Server is running on http://localhost:3000");
+app.listen(3001, () => {
+	console.log("Server is running on http://localhost:3001");
 });
