b08b0f8edebc4c962e643521cbb8518b7d6c3597
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2024-08-30 00:01:53 +0200

    backend: update with new routes

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -3,11 +3,20 @@ import { registerUser } from "./controllers/auth/registerUser";
 import { loginUser } from "./controllers/auth/loginUser";
 import { createTask } from "./controllers/tasks/createTask";
 import { authenticateToken } from "./middlewares/auth";
+import { getTasks } from "./controllers/tasks/getTasks";
+import { deleteTask } from "./controllers/tasks/deleteTask";
+import { updateTask } from "./controllers/tasks/updateTask";
 
 const router = express.Router();
 
+// Auth
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// Tasks
 router.post("/tasks", authenticateToken, createTask);
+router.get("/tasks/:userId", getTasks);
+router.delete("/tasks/:userId/:taskId", deleteTask);
+router.put("/task", updateTask);
 
 export default router;
