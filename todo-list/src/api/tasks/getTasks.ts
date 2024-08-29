f6cce975245c9b7c5e0c009c882cd1b1ff8d0aa8
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2024-08-30 00:25:08 +0200

    frontend: fix getTasks

--- todo-list/src/api/tasks/getTasks.ts
+++ todo-list/src/api/tasks/getTasks.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-export const getTasks = async (token: string) => {
-	const response = await axios.get(`${API_URL}/tasks`, {
+export const getTasks = async (token: string, userId: string) => {
+	const response = await axios.get(`${API_URL}/tasks/${userId}`, {
 		headers: {
 			Authorization: `Bearer ${token}`,
 		},
