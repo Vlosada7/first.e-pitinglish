e63cf546a410d5a8f1f95bfd7c2c36a21d8a2428
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2024-08-29 21:29:35 +0200

    update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,38 +1,33 @@
 import express from "express";
-import os from "os";
 import router from "./router";
 import cors from "cors";
 import dotenv from "dotenv";
+import axios from "axios"; // Instale com `npm install axios`
 dotenv.config();
-
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use(router);
 
 const port = process.env.PORT || 3001;
-
-// Função para capturar o endereço IP da máquina
-const getLocalIPAddress = () => {
-	const interfaces = os.networkInterfaces();
-	for (const name of Object.keys(interfaces)) {
-		for (const iface of interfaces[name]!) {
-			if (iface.family === "IPv4" && !iface.internal) {
-				return iface.address; // Retorna o IP externo (não localhost)
-			}
-		}
-	}
-	return "localhost";
-};
 
 // Rota para exibir "Servido" na página principal
 app.get("/", (req, res) => {
 	res.send("Servidor");
 });
 
-app.listen(port, () => {
-	const ipAddress = getLocalIPAddress();
-	console.log(`Server is running on http://${ipAddress}:${port}`);
+const getPublicIPAddress = async () => {
+	try {
+		const response = await axios.get("https://api.ipify.org?format=json");
+		return response.data.ip;
+	} catch (error) {
+		console.error("Não foi possível obter o IP público:", error);
+		return "localhost";
+	}
+};
+
+app.listen(port, async () => {
+	const publicIPAddress = await getPublicIPAddress();
+	console.log(`Server is running on http://${publicIPAddress}:${port}`);
 });
