9554c73c90846873341a1b18d1234d6ee8741e8b
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2024-08-30 02:13:29 +0200

    frontend: layout with spinner

--- todo-list/src/components/Home/Home.tsx
+++ todo-list/src/components/Home/Home.tsx
@@ -4,13 +4,14 @@ import { createTask } from "../../api/tasks/createTask";
 import { getTasks } from "../../api/tasks/getTasks";
 import { updateTask } from "../../api/tasks/updateTask";
 import { deleteTask } from "../../api/tasks/deleteTask";
+import Spinner from "../Spinner/Spinner";
 
 interface Task {
 	id: number;
 	title: string;
 	description: string;
 	dueDate: string;
-	completed: boolean;
+	isCompleted: boolean;
 	userId: number;
 }
 
@@ -35,9 +36,6 @@ const formatDatetime = (datetime: string | undefined) => {
 	return `${day}/${month}/${year} - ${hours}:${minutes}`;
 };
 
-// Teste a função com uma data ISO fixa
-console.log(formatDatetime("2024-12-12T12:12:00Z"));
-
 const Home: React.FC = () => {
 	const [title, setTitle] = useState("");
 	const [description, setDescription] = useState("");
@@ -45,53 +43,68 @@ const Home: React.FC = () => {
 	const [tasks, setTasks] = useState<Task[]>([]);
 	const userId = localStorage.getItem("userId");
 	const token = localStorage.getItem("authToken");
+	const [loadingCreateTask, setLoadingCreateTask] = useState(false);
+	const [loadingTasks, setLoadingTasks] = useState<boolean[]>([]);
+	const [error, setError] = useState("");
 
 	// Lógica para adicionar
 	const handleAddTask = async (e: React.FormEvent) => {
 		e.preventDefault();
+		setLoadingCreateTask(true);
 
 		if (title && datetime && token) {
-			const formattedDatetime = new Date(datetime).toISOString();
-			const newTask: Task = {
-				id: 0, // O ID real será atribuído pelo backend
-				title,
-				description,
-				dueDate: formattedDatetime,
-				userId: Number(userId),
-				completed: false,
-			};
-			await createTask(token, newTask);
+			try {
+				const formattedDatetime = new Date(datetime).toISOString();
+				const newTask: Task = {
+					id: 0, // O ID real será atribuído pelo backend
+					title,
+					description,
+					dueDate: formattedDatetime,
+					userId: Number(userId),
+					isCompleted: false,
+				};
+				await createTask(token, newTask);
 
-			setTasks(
-				[...tasks, newTask].sort(
-					(a, b) =>
-						new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime()
-				)
-			);
-			setTitle("");
-			setDescription("");
-			setDatetime("");
+				setTasks(
+					[...tasks, newTask].sort(
+						(a, b) =>
+							new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime()
+					)
+				);
+				setTitle("");
+				setDescription("");
+				setDatetime("");
+			} catch (error) {
+				setError("Problema ao criar a task");
+			} finally {
+				setLoadingCreateTask(false);
+			}
 		}
 	};
 
 	// Lógica para pegar as tasks
 	useEffect(() => {
 		const userTasks = async () => {
+			setLoadingCreateTask(true);
 			try {
 				if (token && userId) {
 					const response = await getTasks(token, userId);
 					setTasks(response.tasks);
-					console.log(response.tasks);
 				}
 			} catch (error) {
 				console.error(error);
+			} finally {
+				setLoadingCreateTask(false);
 			}
 		};
 		userTasks();
 	}, [token, userId]);
 
-	// Lógica para atualizar (toggle `completed`)
-	const handleToggleComplete = async (task: Task) => {
+	// Lógica para atualizar (toggle isCompleted)
+	const handleToggleComplete = async (task: Task, index: number) => {
+		const newLoadingTasks = [...loadingTasks];
+		newLoadingTasks[index] = true;
+		setLoadingTasks(newLoadingTasks);
 		try {
 			if (token && userId) {
 				const updatedTask = await updateTask(token, {
@@ -101,74 +114,136 @@ const Home: React.FC = () => {
 
 				// Atualize a lista de tarefas localmente
 				const updatedTasks = tasks.map((t) =>
-					t.id === task.id ? { ...t, completed: updatedTask.isCompleted } : t
+					t.id === task.id ? { ...t, isCompleted: updatedTask.isCompleted } : t
 				);
 				setTasks(updatedTasks);
 			}
 		} catch (error) {
 			console.error("Erro ao atualizar tarefa:", error);
+		} finally {
+			newLoadingTasks[index] = false;
+			setLoadingTasks(newLoadingTasks);
 		}
 	};
 
-	// Lógica para deletar as tasks (ainda não implementada no backend)
-	const handleDeleteTask = async (taskId: number) => {
+	// Lógica para deletar as tasks
+	const handleDeleteTask = async (taskId: number, index: number) => {
+		const newLoadingTasks = [...loadingTasks];
+		newLoadingTasks[index] = true;
+		setLoadingTasks(newLoadingTasks);
 		try {
 			if (token && userId) {
-				await deleteTask(token, userId, taskId.toString()); // Chama a API deleteTask
+				await deleteTask(token, userId, taskId.toString());
 
 				// Remove a tarefa deletada do estado local
 				const updatedTasks = tasks.filter((task) => task.id !== taskId);
 				setTasks(updatedTasks);
+				newLoadingTasks.splice(index, 1); // Remove o estado de loading da task deletada
+				setLoadingTasks(newLoadingTasks);
 			}
 		} catch (error) {
 			console.error("Erro ao deletar tarefa:", error);
 		}
 	};
 
+	// Filtra as tarefas pendentes e concluídas
+	const pendingTasks = tasks.filter((task) => !task.isCompleted);
+	const completedTasks = tasks.filter((task) => task.isCompleted);
+
 	return (
 		<div className="home-container">
 			<div className="task-form">
 				<h2>Agregar nueva tarea</h2>
-				<form onSubmit={handleAddTask}>
-					<input
-						type="text"
-						placeholder="Título"
-						value={title}
-						onChange={(e) => setTitle(e.target.value)}
-						required
-					/>
-					<input
-						type="text"
-						placeholder="Descripción (opcional)"
-						value={description}
-						onChange={(e) => setDescription(e.target.value)}
-					/>
-					<input
-						type="datetime-local"
-						value={datetime}
-						onChange={(e) => setDatetime(e.target.value)}
-						required
-					/>
-					<button type="submit">Agregar</button>
-				</form>
+				{loadingCreateTask ? (
+					<Spinner />
+				) : (
+					<form onSubmit={handleAddTask}>
+						<input
+							type="text"
+							placeholder="Título"
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
+							required
+						/>
+						<input
+							type="text"
+							placeholder="Descripción (opcional)"
+							value={description}
+							onChange={(e) => setDescription(e.target.value)}
+						/>
+						<input
+							type="datetime-local"
+							value={datetime}
+							onChange={(e) => setDatetime(e.target.value)}
+							required
+						/>
+						<button type="submit">Agregar</button>
+					</form>
+				)}
 			</div>
+
 			<div className="task-list">
-				<h2>Tareas</h2>
-				{tasks.length > 0 ? (
+				<h2>Tareas Pendientes</h2>
+				{pendingTasks.length > 0 ? (
 					<ul>
-						{tasks.map((task, index) => (
-							<li key={index} className={task.completed ? "completed" : ""}>
-								<input
-									type="checkbox"
-									checked={task.completed}
-									onChange={() => handleToggleComplete(task)}
-								/>
-								<h3>{task.title}</h3>
-								<p>{task.description}</p>
-								<time>{formatDatetime(task.dueDate)}</time>
-								<button onClick={() => handleDeleteTask(task.id)}>
-									Excluir
-								</button>
+						{pendingTasks.map((task, index) => (
+							<li key={index} className="task-item">
+								{loadingTasks[index] ? (
+									<Spinner />
+								) : (
+									<>
+										<div className="task-info">
+											<h3>{task.title}</h3>
+											<p>{task.description}</p>
+										</div>
+										<div className="task-actions">
+											<time>{formatDatetime(task.dueDate)}</time>
+											<input
+												type="checkbox"
+												checked={task.isCompleted}
+												onChange={() => handleToggleComplete(task, index)}
+											/>
+											<button onClick={() => handleDeleteTask(task.id, index)}>
+												Excluir
+											</button>
+										</div>
+									</>
+								)}
+							</li>
+						))}
+					</ul>
+				) : (
+					<p>No se agregaron tareas.</p>
+				)}
+			</div>
+
+			<div className="task-list">
+				<h2>Tareas Completadas</h2>
+				{completedTasks.length > 0 ? (
+					<ul>
+						{completedTasks.map((task, index) => (
+							<li key={index} className="task-item completed">
+								{loadingTasks[index] ? (
+									<Spinner />
+								) : (
+									<>
+										<div className="task-info">
+											<h3>{task.title}</h3>
+											<p>{task.description}</p>
+										</div>
+										<div className="task-actions">
+											<time>{formatDatetime(task.dueDate)}</time>
+											<input
+												type="checkbox"
+												checked={task.isCompleted}
+												onChange={() => handleToggleComplete(task, index)}
+											/>
+											<button onClick={() => handleDeleteTask(task.id, index)}>
+												Excluir
+											</button>
+										</div>
+									</>
+								)}
 							</li>
 						))}
 					</ul>
